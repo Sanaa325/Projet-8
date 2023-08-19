4127c5efce313c9ec60030c4b811ecc32ece4d7c
Sanaa <ahdoursanaa@gmail.com>
2023-08-19 15:48:31 +0400

    commit

--- src/Components/Collapse/Collapse.js
+++ src/Components/Collapse/Collapse.js
@@ -1,13 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import classes from "./Collapse.module.css";
+import chevronUp from "../../Images/chevronUp.png";
+import chevronDown from "../../Images/chevronDown.png";
 
 
 
 
 function Collapse() {
 
-    return 
-        
-    
+    const [isOpen, setIsOpen] = useState(false);
+    const displayContent = () => {
+        setIsOpen(!isOpen)
+    }
+
+    const collapseContent = `${classes.Collapse} ${isOpen ? classes.visible : classes.hidden}`;
+    const chevron = (isOpen ? chevronUp : chevronDown);
+
+
+    return (
+        <div className={classes.Collapse}>
+            <div onClick={displayContent} >
+                <span>{title}</span>
+            </div>
+            <div className={classes.chevron}>
+            <img src={chevron} alt="" />
+            </div>
+
+            <div className={collapseContent}>
+                blablabdcnjsnnvjdxnvofnvofnvosdmzmvpdfmvpfm
+
+            </div>
+
+
+
+        </div>
+
+    )
+
+
 
 
 
--- src/Containers/About/About.js
+++ src/Containers/About/About.js
@@ -1,13 +1,31 @@
 import React from "react";
 import Banner from "../../Components/Banner/Banner";
-
+import Collapse from "../../Components/Collapse/Collapse.js"
 
 
 
 function About() {
+const fiabilité = "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes."
+const respect = "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
+const service = "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
+const sécurité = "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
+
+
     return (
         <>
             <Banner />
+            <div>
+                <Collapse title="Fiabilité" content={fiabilité} /> 
+            </div>
+            <div>
+                <Collapse title="Respect" content={respect} /> 
+            </div>
+            <div>
+                <Collapse title="Service" content={service} /> 
+            </div>
+            <div>
+                <Collapse title="Sécurité" content={sécurité} /> 
+            </div>
 
         </>
     )
