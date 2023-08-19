6e839ec020913ef6844de9c071bfd71d0e72e159
Sanaa <ahdoursanaa@gmail.com>
2023-08-19 18:48:27 +0400

    commit 11

---
+++ src/Components/Slideshow/Slideshow.js
@@ -0,0 +1,19 @@
+import react from "react";
+import chevronLeft from "../../Images/chevronLeft.png";
+import chevronRight from "../../Images/chevronRight.png";
+import classes from "../Slideshow/Slideshow.module.css";
+
+function Slideshow() {
+    return
+}
+
+
+
+
+
+
+
+
+
+
+export default Slideshow;
--- src/Containers/Cards/DetailCard/DetailCard.js
+++ src/Containers/Cards/DetailCard/DetailCard.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { useParams } from "react-router-dom";
 import logements from '../../../logements.json'
+import Slideshow from "../../../Components/Slideshow/Slideshow";
 
 function DetailCard() {
     const {id}=useParams()
