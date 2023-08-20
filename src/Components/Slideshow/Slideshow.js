2da49885a8e61d04542e58e37c1a089283b7f092
Sanaa <ahdoursanaa@gmail.com>
2023-08-20 14:35:15 +0400

    commit 12

--- src/Components/Slideshow/Slideshow.js
+++ src/Components/Slideshow/Slideshow.js
@@ -3,8 +3,18 @@ import chevronLeft from "../../Images/chevronLeft.png";
 import chevronRight from "../../Images/chevronRight.png";
 import classes from "../Slideshow/Slideshow.module.css";
 
-function Slideshow() {
-    return
+function Slideshow({ pictures }) {
+    return (
+
+        <div className={classes.Carrousel}>
+            <div>
+                <img src=""/>
+
+
+            </div>
+        </div>
+
+    )
 }
 
 
