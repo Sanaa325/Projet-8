35b3bc54fcf14fee1f0b0f4b9cf3b46908385040
Sanaa <ahdoursanaa@gmail.com>
2023-08-20 17:00:06 +0400

    commit

--- src/Components/Slideshow/Slideshow.js
+++ src/Components/Slideshow/Slideshow.js
@@ -13,20 +13,24 @@ function Slideshow({ pictures }) {
 
     return (
 
+
         <div className={classes.Carrousel}>
             <div>
+                {/*Affiche la première image*/}
                 <img className={classes.imgCarr} src={pictures[index]} key={"image" + index} alt={"image" + index} />
             </div>
 
-
+            {/* Si plus d'une image, affichage des chevrons de nav*/}
             {maxPictures > 0 && (
                 <div>
-                    <button className={classes.slideshowBtn} onClick={()=> setIndex(index - 1)}>{index}
-                        <img className={classes.chevronLeft} src={chevronLeft} alt={"flèche gauche pour changer image"+index}/>
-                    </button> 
-
-                    <button className={classes.slideshowBtn} onClick={()=> setIndex(index + 1)}>{index}
-                        <img className={classes.chevronRight} src={chevronRight} alt={"flèche droite pour changer image"+index}/>
+                    {/* Bouton pour passer à l'image précédante */}
+                    <button className={classes.slideshowBtn} onClick={() => setIndex(index - 1)}>{index}
+                        <img className={classes.chevronLeft} src={chevronLeft} alt={"flèche gauche pour changer image" + index} />
+                    </button>
+                    
+                    {/* Bouton pour passer à l'image suivante */}
+                    <button className={classes.slideshowBtn} onClick={() => setIndex(index + 1)}>{index}
+                        <img className={classes.chevronRight} src={chevronRight} alt={"flèche droite pour changer image" + index} />
                     </button>
                 </div>
             )}
