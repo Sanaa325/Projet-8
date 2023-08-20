3884626a1421799cdb05067ba83e44f4692e0054
Sanaa <ahdoursanaa@gmail.com>
2023-08-20 16:26:26 +0400

    commit

--- src/Components/Slideshow/Slideshow.js
+++ src/Components/Slideshow/Slideshow.js
@@ -4,14 +4,38 @@ import chevronRight from "../../Images/chevronRight.png";
 import classes from "../Slideshow/Slideshow.module.css";
 
 function Slideshow({ pictures }) {
+
     const [index, setIndex] = useState(0)
+    const maxPictures = pictures.length - 1
+
+    if (index > maxPictures) setIndex(0)
+    if (index < 0) setIndex(maxPictures)
+
     return (
 
         <div className={classes.Carrousel}>
             <div>
-                <img src={pictures[index]} key={"image"+index} alt={"image"+index} />
+                <img className={classes.imgCarr} src={pictures[index]} key={"image" + index} alt={"image" + index} />
             </div>
+
+
+            {maxPictures > 0 && (
+                <div>
+                    <button className={classes.slideshowBtn} onClick={()=> setIndex(index - 1)}>{index}
+                        <img className={classes.chevronLeft} src={chevronLeft} alt={"flèche gauche pour changer image"+index}/>
+                    </button> 
+
+                    <button className={classes.slideshowBtn} onClick={()=> setIndex(index + 1)}>{index}
+                        <img className={classes.chevronRight} src={chevronRight} alt={"flèche droite pour changer image"+index}/>
+                    </button>
+                </div>
+            )}
         </div>
+
+
+
+
+
 
     )
 }
